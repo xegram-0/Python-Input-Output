e7b1c0318628be071f2c5af93d80e71a4ab57723
Hong <nguyenhongnhan9a5@gmail.com>
2024-04-18 09:44:15 +0700

    e1 solved

---
+++ Exercise 1- Accept numbers from a user.py
@@ -0,0 +1,7 @@
+# Exercise 1: Accept numbers from a user
+def multiplication(num1, num2):
+    return num1*num2
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+print("The result:", multiplication(num1, num2))
